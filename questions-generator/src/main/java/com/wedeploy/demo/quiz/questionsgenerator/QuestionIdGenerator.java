d7bd22dbb95516d49f5b590928a76dbc37eba1ac
Igor Spasić <igor.spasic@liferay.com>
2016-11-09 14:22:51 +0100

    Fixed ID generation for questions.

---
+++ questions-generator/src/main/java/com/wedeploy/demo/quiz/questionsgenerator/QuestionIdGenerator.java
@@ -0,0 +1,15 @@
+package com.wedeploy.demo.quiz.questionsgenerator;
+
+public class QuestionIdGenerator {
+
+	private int id;
+
+	public QuestionIdGenerator(int initialValue) {
+		this.id = initialValue;
+	}
+
+	public int nextId() {
+		return id++;
+	}
+
+}
