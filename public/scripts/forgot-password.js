289deb8ccab2ffc82c1ecf0f29249ff90702ca85
ygor.costa <ygor.costa@liferay.com>
2016-11-10 13:34:26 -0300

    Changes forms feedback

--- public/scripts/forgot-password.js
+++ public/scripts/forgot-password.js
@@ -1,11 +1,19 @@
 const DOMAIN = window.location.hostname.split(".").slice(-3).join(".");
 const auth = WeDeploy.auth('auth.' + DOMAIN);
+const alert = document.getElementById('alert');
 
 function resetPassword() {
 	WeDeploy.auth('auth.' + DOMAIN)
 		.sendPasswordResetEmail(forgotPassword.email.value)
 		.then(() => {
-			alert('An email with instructions to reset password should arrive in a few minutes.');
-			location.href = '/login.html';
+      alert.innerHTML = '<p>An email with instructions to reset password should arrive in a few minutes.</p>';
+      alert.innerHTML += '<button><span class="close icon-12-close-short" onclick="closeAlert()"></span></button>';
+      alert.classList.add('visible');
+
+			//location.href = '/login.html';
 		});
+}
+
+function closeAlert() {
+  location.href = '/login.html';
 }
